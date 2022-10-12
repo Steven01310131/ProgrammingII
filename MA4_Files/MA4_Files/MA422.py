2f5a71a5b5b357a30792b2400c8eb12626473680
Steven01310131 <stefanos.tsabanakis@gmail.com>
2022-10-12 14:47:54 +0200

    finished

--- MA4_Files/MA4_Files/MA422.py
+++ MA4_Files/MA4_Files/MA422.py
@@ -5,44 +5,55 @@ from numba import njit
 
 def main():
 	
-	pythonTimes = []
-	cplusplustimes = []
-	nlist = [30,35,36,37,40,41] # enter 30,40,45 for the long run
+	purepythontime= []
+	clibrarytime= []
+	numbatime=[]
+	nlist = [30,35,36,37,40,41] 
 	for n in nlist:
+		print("for n=",n)
 		#Runnning with pure python
 		time_start1 = time.perf_counter()
-		result = fib_numba(n)
+		result = fib_py(n)
 		time_stop1 = time.perf_counter()
-		#pythonTimes.append(time_stop1-time_start1)
-		print("For n=",n,"time passed is ",time_stop1-time_start1)
-		#Running with C++
+		purepythontime.append(time_stop1-time_start1)
+		print("For n=",n,"in pure python time required is ",time_stop1-time_start1)
+
+		#Running with numba 
 		time_start2 = time.perf_counter()
+		result = fib_numba(n)
+		time_stop2 = time.perf_counter()
+		numbatime.append(time_stop2-time_start2)
+		print("For n=",n,"in python using numba time required is ",time_stop2-time_start2)
+
+		#Running with C++
+		time_start3= time.perf_counter()
 		f = Person(n)
 		f.fib()
-		time_stop2 = time.perf_counter()
-		print("n is", n)
-		cplusplustimes.append(time_stop2-time_start2)
-		# print("C++ Fibonacci Execution /n", "The result is = Time elapsed = ", time_stop2-time_start2, "seconds")
-	# print("The length of the lists is", len(pythonTimes))
-	# #print(pythonTimes)
-	# plt.plot(nlist, pythonTimes, color = 'red', label = "Python")
-	# plt.plot(nlist, cplusplustimes, color = 'blue', label = "C++")
-	# plt.xlabel("Fibonacci number n")
-	# plt.ylabel("Time (s)")
-	# plt.legend()
-	# plt.xlim(0 , 45)
-	# plt. ylim(0, 120)
-	# plt.show()
-	# plt.savefig('timeplot6.png')
+		time_stop3= time.perf_counter()
+		clibrarytime.append(time_stop3-time_start3)
+		print("For n=",n,"in python using c++ time required is ",time_stop3-time_start3)
+		print()
+		
+	plt.plot(nlist, purepythontime, color = 'red', label = "Python")
+	plt.plot(nlist, numbatime, color = 'blue', label = "Numba")
+	plt.plot(nlist, clibrarytime, color = 'green', label = "C++")
+	plt.xlabel("Fibonacci number n")
+	plt.ylabel("Time (s)")
+	plt.legend()
+	plt.xlim(30, 45)
+	plt.show()
+	plt.savefig('timeplot.png')
 
-	# #C++ Fibonacci Execution  /n The result is =  -1323752223 Time elapsed =  57.004970107926056 seconds
-	# #the number is longer than int
-	# #Fibonacci Calculation 47
 	# time_start = time.perf_counter()
-	# f = Integer(47)
+	# f = Person(47)
 	# result = f.fib()
 	# time_stop = time.perf_counter()
 	# print("C++ Fibonacci Execution  /n", "The result is = ", result, "Time elapsed = ", time_stop-time_start, "seconds")
+	"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+	"Output in terminal for n=47 with C++ execution                                                     "
+	"C++ Fibonacci Execution  /n The result is =  -1323752223 Time elapsed =  20.691632530999414 seconds"
+	"                                                                                                   "
+	"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 def fib_py(n):
 	if n <= 1:
 		return n
